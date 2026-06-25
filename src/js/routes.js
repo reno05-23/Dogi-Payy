06dd262527602cc2192a2a788a1ddf9cf4f48c1d
Fanani Fatihul <fananifatihul489@gmail.com>
2026-06-25 14:52:37 +0700

    'halaman'

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,16 +1,46 @@
-import Beranda from '../pages/beranda.f7';
 import Home from '../pages/home.f7';
+import Beranda from '../pages/beranda.f7';
 import Tentang from '../pages/tentang.f7';
+import Transfer from '../pages/transfer.f7';
+import Qris from '../pages/qris.f7';
+import Tabungan from '../pages/tabungan.f7';
+import Profil from '../pages/profile.f7';
+import History from '../pages/history.f7';
+import Topup from '../pages/topup.f7';
+import Notifikasi from '../pages/notifikasi.f7';
 
 var routes = [
     {
         path: '/',
         component: Home,
         tabs: [
-            {path: '/', id: 'view-beranda', component: Beranda},
-            {path: '/tentang/', id: 'view-tentang', component: Tentang}
+            { path: '/', id: 'view-beranda', component: Beranda },
+            { path: '/transfer/', id: 'view-transfer', component: Transfer },
+            { path: '/qris/', id: 'view-qris', component: Qris },
+            { path: '/tabungan/', id: 'view-tabungan', component: Tabungan },
+            { path: '/profil/', id: 'view-profil', component: Profil }
         ]
     },
+    {
+      path: '/tentang/',
+        component: Tentang,
+    },
+    {
+      path: '/notifikasi/',
+      component: Notifikasi,
+    },
+    {
+      path: '/history/',
+      popup:{
+        component: History,
+      },
+    },
+    {
+      path: '/topup/',
+      popup:{
+        component: Topup,
+      },
+    }
 ];
 
 export default routes;
